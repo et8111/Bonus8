2e8846e8f9202850a6ff2a751d82a7ae625984df
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-11-13 16:59:21 -0500

    First push of website

---
+++ test4/Scripts/PenguinScript.js
@@ -0,0 +1,28 @@
+﻿/*
+pTitle
+pH1
+pP
+itemsID
+list1
+list2
+list3
+list4
+HID         1
+pLi1
+pLi2
+pButton
+*/
+
+function PENGUINIFY()
+{
+    document.title = "POV!";
+    document.getElementById("pH1").innerHTML = "Unleash The Pebbles.";
+    document.getElementById("pP").innerHTML = "Penguin This, Penguin That.";
+    $('#bp2').css('background-image', 'url(https://images.unsplash.com/photo-1508967289497-b9c85158e02d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=956a875807ae3d1b502cbd6586c7065a&auto=format&fit=crop&w=1990&q=80)');
+    document.getElementById("itemsID").innerHTML = "\"Now where there south side of earth wassup, wassup";
+    document.getElementById("list1").innerHTML = "Where the water world birds, put your water wings up";
+    document.getElementById("list2").innerHTML = "Where my Emu birds that's running the plains";
+    document.getElementById("list3").innerHTML = "And all my birds flying north thats doin' a bid";
+    document.getElementById("list4").innerHTML = "Oh yeah im rare, im aware that I'm rare<br/>I waddle and I'm real, I'm of the few here\"";
+    document.getElementById("HID").innerHTML = "P";
+}
---
+++ test4/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="penguinscript.js" />
+/// <reference path="respond.js" />
