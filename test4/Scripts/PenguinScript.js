d9f5b682fcb2850c5a086da7ec06c5efb52ef951
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-11-13 21:22:15 -0500

    finished product?

--- test4/Scripts/PenguinScript.js
+++ test4/Scripts/PenguinScript.js
@@ -15,16 +15,21 @@ pButton
 
 function PENGUINIFY()
 {
-    document.getElementById("pH1").innerHTML = "Unleash The Pebbles.";
-    $("#pH1").fadeOut(1);
-    $("#pH1").fadeIn(5000);
-    document.title = "POV!";
-    document.getElementById("pP").innerHTML = "Penguin This, Penguin That.";
-    $('#bp2').css('background-image', 'url(https://images.unsplash.com/photo-1508967289497-b9c85158e02d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=956a875807ae3d1b502cbd6586c7065a&auto=format&fit=crop&w=1990&q=80)');
-    document.getElementById("itemsID").innerHTML = "\"Now where there south side of earth wassup, wassup";
-    document.getElementById("list1").innerHTML = "Where the water world birds, put your water wings up";
-    document.getElementById("list2").innerHTML = "Where my Emu birds that's running the plains";
-    document.getElementById("list3").innerHTML = "And all my birds flying north thats doin' a bid";
-    document.getElementById("list4").innerHTML = "Oh yeah im rare, im aware that I'm rare<br/>I waddle and I'm real, I'm of the few here\"";
-    document.getElementById("HID").innerHTML = "P";
+    if (document.title == "POV!")
+        location.reload();
+    else {
+        document.getElementById("pH1").innerHTML = "Unleash The Pebbles.";
+        $("#pH1").fadeOut(1);
+        $("#pH1").fadeIn(3000);
+        document.title = "POV!";
+        document.getElementById("pP").innerHTML = "Penguin This, Penguin That.";
+        $('#bp2').css('background-image', 'url(https://images.unsplash.com/photo-1508967289497-b9c85158e02d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=956a875807ae3d1b502cbd6586c7065a&auto=format&fit=crop&w=1990&q=80)');
+        document.getElementById("itemsID").innerHTML = "\"Now where there south side of earth wassup, wassup";
+        document.getElementById("list1").innerHTML = "Where the water world birds, put your water wings up";
+        document.getElementById("list2").innerHTML = "Where my Emu birds that's running the plains";
+        document.getElementById("list3").innerHTML = "And all my birds flying north thats doin' a bid";
+        document.getElementById("list4").innerHTML = "Oh yeah im rare, im aware that I'm rare<br/>I waddle and I'm real, I'm of the few here\"";
+        document.getElementById("HID").innerHTML = "P";
+        document.getElementById("pButton").innerText = "BACK";
+    }
 }
